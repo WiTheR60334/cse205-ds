99642d10986211895c2854f49cbb75f737a9fc74
WiTheR60334 <enderwither1234@gmail.com>
2023-08-14 15:02:59 +0530

    updated

---
+++ Assignments/14.08.2023/344.py
@@ -0,0 +1,12 @@
+class Solution:
+    def revStr(self, i, n, s):
+        if i >= n // 2:
+            return
+        s[i], s[n - 1 - i] = s[n - 1 - i], s[i]
+        self.revStr(i + 1, n, s)
+
+    def reverseString(self, s):
+        n = len(s)
+        self.revStr(0, n, s)
+
+
