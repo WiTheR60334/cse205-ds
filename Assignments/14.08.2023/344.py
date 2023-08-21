36172ed18b910b72c142237f1f5e04808945b542
WiTheR <115364885+WiTheR60334@users.noreply.github.com>
2023-08-21 13:11:12 +0530

    Update 344.py

--- Assignments/14.08.2023/344.py
+++ Assignments/14.08.2023/344.py
@@ -1,16 +1,11 @@
 class Solution:
-    def revStr(self, i, n, s):
-        if i >= n // 2:
-            return
-        s[i], s[n - 1 - i] = s[n - 1 - i], s[i]
-        self.revStr(i + 1, n, s)
-
     def reverseString(self, s):
-        n = len(s)
-        self.revStr(0, n, s)
-
-# def rev(r):
-#     return r[::-1]
-# a = ["h", "e", "l", "l", "o"]
-# print(rev(a))
-
+        end = len(s) - 1
+        def recursiveReverse(s, i):
+            if i < len(s)/2:
+                s[i], s[end-i] = s[end-i],s[i]
+                i =  i + 1
+                recursiveReverse(s,i)
+            else:
+                return
+        recursiveReverse(s,0)
