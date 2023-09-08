ed5acc99e53d9c409bdedde0eeeaaad26c783d65
WiTheR60334 <enderwither1234@gmail.com>
2023-09-08 18:28:19 +0530

    updated

--- Assignments/04-09-2023/234.py
+++ Assignments/04-09-2023/234.py
@@ -1,22 +1,18 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        temp1,temp2 = head,head
-
-        while temp2 and temp2.next:
-            temp1 = temp1.next
-            temp2 = temp2.next.next
-
-        prev = None
-        while temp1:
-            nxt = temp1.next
-            temp1.next = prev
-            prev = temp1
-            temp1 = nxt
-
-        while prev:
-            if prev.val != head.val:
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        rev=None
+        while slow:
+            nxt=slow.next
+            slow.next=rev
+            rev=slow
+            slow=nxt
+        while rev:
+            if rev.val!=head.val:
                 return False
-            prev = prev.next
-            head = head.next
-
-        return True
+            rev=rev.next
+            head=head.next
+        return True 
