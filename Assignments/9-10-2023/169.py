c1fcc29d8b887a891097816c1011e9ff2bdec58d
WiTheR60334 <enderwither1234@gmail.com>
2023-10-11 12:26:30 +0530

    updated

--- Assignments/9-10-2023/169.py
+++ Assignments/9-10-2023/169.py
@@ -1,7 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        sol = 0
-        cnt = 0
+        sol,cnt = 0,0
         for i in nums:
             if cnt == 0:
                 sol = i
